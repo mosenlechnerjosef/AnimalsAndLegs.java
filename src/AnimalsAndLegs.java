07f6003aef8115dc90e3d0d0f4513e3a10a63f6a
mosenlechnerjosef <szt2mosjos@vasvari.org>
2023-09-15 20:08:15 +0200

    kész

---
+++ src/AnimalsAndLegs.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class AnimalsAndLegs {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter the number of chickens: ");
+        int numChickens = scanner.nextInt();
+        
+        System.out.print("Please enter the number of pigs: ");
+        int numPigs = scanner.nextInt();
+
+        int totalLegs = (numChickens * 2) + (numPigs * 4);
+
+        System.out.println("The number of legs: " + totalLegs);
+
+        scanner.close();
+    }
+}
