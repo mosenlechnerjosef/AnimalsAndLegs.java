45b5ec1464e2dcd4808dca4bbbdbd95e3aae09d1
mosenlechnerjosef <szt2mosjos@vasvari.org>
2023-09-16 12:56:17 +0200

    kész

---
+++ src/BasicInfo.java
@@ -0,0 +1,15 @@
+public class BasicInfo {
+    public static void main(String[] args) {
+     
+        String name = "Mösenlechner Josef";
+        int age = 19; 
+        double height = 1.85;
+        boolean isMarried = false; 
+
+        
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height (meters): " + height);
+        System.out.println("Married: " + isMarried);
+    }
+}
